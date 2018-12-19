7ba539430d1a9f21db3147f98d0c376b0c297e8e
xiaowheat <eth_dev@126.com>
2018-12-18 22:32:03 -0800

    No ENABLE_MODULE_RECOVERY

--- web3swift/lib/secp256k1/secp256k1.c
+++ web3swift/lib/secp256k1/secp256k1.c
@@ -560,8 +560,9 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 # include "modules/schnorr/main_impl.h"
 #endif
 
+/*
 #ifdef ENABLE_MODULE_RECOVERY
 # include "modules/recovery/main_impl.h"
 #endif
-
+*/
 #pragma clang diagnostic pop
